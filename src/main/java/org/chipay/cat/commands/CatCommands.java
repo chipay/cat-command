733c6d5c6c698ddcd107f36f1d257d43bc829004
German <germans@gmail.com>
2016-02-29 01:13:11 +0000

    Added command to save file. Issue with Jackson mapping, no image is returned

--- src/main/java/org/chipay/cat/commands/CatCommands.java
+++ src/main/java/org/chipay/cat/commands/CatCommands.java
@@ -1,11 +1,19 @@
 package org.chipay.cat.commands;
 
+import org.apache.commons.io.FileUtils;
 import org.chipay.cat.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.annotation.CliAvailabilityIndicator;
 import org.springframework.shell.core.annotation.CliCommand;
+import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
 
 @Component
 public class CatCommands implements CommandMarker {
@@ -20,7 +28,11 @@ public class CatCommands implements CommandMarker {
 
     @CliAvailabilityIndicator({"cat fact"})
     public boolean isFactAvailable() {
-        //always available
+        return true;
+    }
+
+    @CliAvailabilityIndicator({"cat file"})
+    public boolean isFileAvailable() {
         return true;
     }
 
@@ -29,4 +41,20 @@ public class CatCommands implements CommandMarker {
         return this.catService.getFact();
     }
 
+    @CliCommand(value = "cat file", help = "Gets and saves a random cat image")
+    public String file(
+            @CliOption(key = {"path"}, mandatory = true,
+                    help = "Where you are saving the file?", specifiedDefaultValue = "catImage.jpg")
+            final String path) {
+        try {
+            URL url = this.catService.getImageURL();
+            File file = new File(path);
+            FileUtils.copyURLToFile(url, file);
+            System.out.println("File saved");
+            return url.toString();
+        } catch( Exception e) {
+            System.err.println(e.getMessage());
+            return "Error";
+        }
+    }
 }
